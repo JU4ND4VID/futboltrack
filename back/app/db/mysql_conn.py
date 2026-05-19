047812a2ec5bb2d011be281169d3ffe8058c2404
JU4ND4VID <104099435+JU4ND4VID@users.noreply.github.com>
2026-05-18 21:22:32 -0500

    Delete back directory

--- back/app/db/mysql_conn.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-from config import Config
-
-def get_mysql():
-    return mysql.connector.connect(
-        host=Config.MYSQL_HOST, port=Config.MYSQL_PORT,
-        user=Config.MYSQL_USER, password=Config.MYSQL_PASSWORD,
-        database=Config.MYSQL_DB, charset="utf8mb4"
-    )
